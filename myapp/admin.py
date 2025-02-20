f84c4f8db32ed52176e26647724059241a831e4b
Alejandrobr1 <alejobotero2@hotmail.com>
2025-02-20 18:50:05 -0500

    Se agregan tablas de la BD

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import TLogin, TUsuario
 # Register your models here.
+
+admin.site.register(TUsuario)
+admin.site.register(TLogin)
--- myapp/models.py
+++ myapp/models.py
@@ -2,23 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Tlogin(models.Model):
-    idlogin=models.BigIntegerField
-    fk_iduser=models.ForeignKey(User)
+
+
+class TUsuario(models.Model):
+    nombre=models.CharField(max_length=100)
+    primer_apellido=models.CharField(max_length=100)
+    segundo_apellido=models.CharField(max_length=100)
+    documento=models.CharField(max_length=50)
+    celular=models.CharField(max_length=30)
+    correo=models.CharField(max_length=255)
+    ciudad=models.CharField(max_length=255)
+    direccion=models.CharField(max_length=100)
+    rol=models.BooleanField(blank=False)
+
+    def __str__(self):
+        return self.nombre +' '+ self.primer_apellido +' '+ self.segundo_apellido
+
+class TLogin(models.Model):
+    fk_iduser=models.ForeignKey(TUsuario, on_delete=models.CASCADE)
     contrasena=models.CharField(max_length=100)
-
-
-
-
-    """nombre=models.CharField(max_length=100),
-    primer_apellido=models.CharField(max_length=100),
-    segundo_apellido=models.CharField(max_length=100),
-    documento=models.CharField(max_length=50),
-    celular=models.CharField(max_length=30),
-    correo=models.CharField(max_length=255),
-    ciudad=models.CharField(max_length=255),
-    direccion=models.CharField(max_length=100),
-    rol=models.BooleanField(blank=False)"""
-
-
-
+    def __str__(self):
+        return self.fk_iduser.correo
+        
