0499e645e432b99f532064542422d4c44c58ab1a
Alejandrobr1 <alejobotero2@hotmail.com>
2025-02-20 15:57:05 -0500

    Se agrega primer modelo de BD

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tlogin(models.Model):
+    idlogin=models.BigIntegerField
+    fk_iduser=models.ForeignKey(User)
+    contrasena=models.CharField(max_length=100)
+
+
+
+
+    """nombre=models.CharField(max_length=100),
+    primer_apellido=models.CharField(max_length=100),
+    segundo_apellido=models.CharField(max_length=100),
+    documento=models.CharField(max_length=50),
+    celular=models.CharField(max_length=30),
+    correo=models.CharField(max_length=255),
+    ciudad=models.CharField(max_length=255),
+    direccion=models.CharField(max_length=100),
+    rol=models.BooleanField(blank=False)"""
+
+
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 # Create your views here.
 def home(request):
     return render(request, "index.html")
 
 def loginup(request):
-    return render(request, "login.html")
+    if request.method == 'GET':
+        return render(request,'login.html',{
+        'form':AuthenticationForm
+        })        
+    else:
+        user = authenticate(username=request.POST['correo'], password=request.POST['contrasena'])
+
+        if user is None:
+            return render(request,'login.html', {
+            'form':AuthenticationForm,
+            'error': 'El usuario o contrasena son incorrectos'
+            })    
+        else: 
+            login(request, user)
+            return redirect('especialidades')
+        
+    
 
 def registro3(request):
     return render(request, "registro3.html")
@@ -16,6 +32,8 @@ def registro3(request):
 def especialidades(request):
     return render(request, "especialidades.html")
 
+
+    
 
 
 
@@ -33,7 +51,6 @@ def registro(request):
            login(request, user)
            return redirect('especialidades')
            
-           #return HttpResponse('enviando formulario de usuario nuevo')
            
         except:
            return render(request, 'registro.html', {
