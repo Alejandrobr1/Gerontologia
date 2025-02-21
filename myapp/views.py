0f0f0c0cbc4360c0047a0e888456812c1e5340d0
Alejandrobr1 <alejobotero2@hotmail.com>
2025-02-21 11:21:12 -0500

    App para primera revision codigo simplificado

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from .models import TUsuario, TLogin
 from django.http import HttpResponse
@@ -17,17 +16,14 @@ def somos(request):
 #este método inicia la sesion del usuario
 def loginup(request):
     if request.method == 'GET': #si se hace la petición al servidor entonces este devuelve la vista de login
-        return render(request,'login.html',{
-        'form':AuthenticationForm #AuthenticationForm se encarga de enviar un formulario al contexto de la plantilla
-        })        
+        return render(request,'login.html')        
     else:
         user = authenticate(username=request.POST['correo'], 
                             password=request.POST['contrasena']) # almacena el usuario en una variable user y autentifica el usuario y contraseña enviados
 
         if user is None: #si el usuario y/o contraseña no existen entonces muestra nuevamente la vista login
             return render(request,'login.html', { 
-            'form':AuthenticationForm,
-            'error': 'El usuario o contraseña son incorrectos' #si no coincide el usuario y contraseña, imprime el error y renderiza la vista de login nuevamente
+                    'error': 'El usuario o contraseña son incorrectos' #si no coincide el usuario y contraseña, imprime el error y renderiza la vista de login nuevamente
             })    
         else: #si el usuario existe en la base de datos y ademas coincide la contraseña, se redirige al usuario a la pagina de inicio
             login(request, user)
@@ -42,9 +38,7 @@ def especialidades(request):
 #este metodo retorna registra al usuario
 def registro(request):
     if request.method == 'GET':#si se hace la petición al servidor entonces este devuelve la vista de registro
-        return render(request, 'registro.html', { 
-            'form': UserCreationForm()#UserCrfeationForm se encarga de crear un formulario en el contexto de la plantilla
-        })
+        return render(request, 'registro.html')
     else:
         #A continuación se almacenan lo datos escritos por el usuario en el formulario en variables
         correo = request.POST.get('correo')
@@ -61,16 +55,14 @@ def registro(request):
 
         #Se valida que las contraseñas coincidan
         if contrasena != confirmar_contrasena:
-            messages.error(request, 'Las contraseñas no coinciden.') #Se mostrará un mensaje al usuario que indica que las contraseñas creadas no coinciden
-            return render(request, 'registro.html', { #Se renderiza nuevamente la vista de registro y se crea un nuevo formulario
-                'form': UserCreationForm()
+            return render(request, 'registro.html',{ 
+                    'error': 'Las contraseñas no coinciden' #Error que indica que las contraseñas no coinciden
             })
 
         #Se valida que el correo no esté registrado
         if User.objects.filter(username=correo).exists() or TUsuario.objects.filter(correo=correo).exists(): #Se busca el correo en las tablas de la clase User y TUsuario y si este existe devuelve un error
-            messages.error(request, 'El correo electrónico ya está registrado.') # se imprimirá el error indicando que el usuario o correo electronico ya esta registrado
-            return render(request, 'registro.html', { #Se renderiza nuevamente la vista de registro y se crea un nuevo formulario
-                'form': UserCreationForm()
+            return render(request, 'registro.html',{ 
+                    'error': 'El correo electrónico ya está registrado' #Error que indica que el correo ya esta registrado
             })
 
         try:
@@ -108,9 +100,7 @@ def registro(request):
         except Exception as e:
              
             messages.error(request, f'Error al registrar el usuario: {str(e)}')#Se muestra especificamente el error ocurrido al intentar guardar el usuario y no lograrlo exitosamente
-            return render(request, 'registro.html', {
-                'form': UserCreationForm() #UserCrfeationForm se encarga de crear un formulario en el contexto de la plantilla
-            })
+            return render(request, 'registro.html')
   
 #este metodo cierra la sesion del usuario y redirecciona al usuario a la pagina principal 
 def cerrarSesion(request):
